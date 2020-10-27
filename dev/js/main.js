268eb08d6ef33f40114af69820cd793a494a5ede
Lars <lars@outlook.com>
2020-10-27 17:13:22 +0100

    Added pixel font

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,2 @@
 import './../scss/main.scss'
+import './../scss/fonts.scss'
--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -189,11 +189,18 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/main.js":[function(require,module,exports) {
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"scss/fonts.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./../fonts/w95fa.woff2":[["w95fa.6ff06818.woff2","fonts/w95fa.woff2"],"fonts/w95fa.woff2"],"./../fonts/w95fa.woff":[["w95fa.db38cd0a.woff","fonts/w95fa.woff"],"fonts/w95fa.woff"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/main.js":[function(require,module,exports) {
 "use strict";
 
 require("./../scss/main.scss");
-},{"./../scss/main.scss":"scss/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+require("./../scss/fonts.scss");
+},{"./../scss/main.scss":"scss/main.scss","./../scss/fonts.scss":"scss/fonts.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -221,7 +228,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56051" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59196" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
