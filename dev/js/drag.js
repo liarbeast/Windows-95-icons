645c328e59a7ac4f31f0441778590476a979ccbc
Lars <lars@outlook.com>
2020-10-30 22:13:31 +0100

    Added hovercursor and fixed icon center

--- dev/js/drag.js
+++ dev/js/drag.js
@@ -4,6 +4,7 @@ var Draggable = require ('Draggable');
 var element = document.getElementById('drag');
 var handle = document.getElementById('drag__handle');
 var options = {
-    handle: drag__handle
+    handle: drag__handle,
   };
+
 new Draggable (element, options);
